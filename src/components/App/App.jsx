0e0e611b45fc7dbc4ff94a2e5f7e85719fd1810f
Simeon Willard <swillard4597@gmail.com>
2021-01-15 16:09:33 -0600

    setGalleryList to fetched data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [galleryList, setGalleryList] = useState([]);
+
+  // lets make fetch happen
+  const fetchGallery = () => {
+    axios.get('/gallery')
+    .then((response) => {
+      console.log(response.data);
+      setGalleryList(response.data);
+    })
+    .catch(function (error) {
+      console.log('Error in get: ', error);
+    });
+  }
+
+
     return (
       <div className="App">
         <header className="App-header">
